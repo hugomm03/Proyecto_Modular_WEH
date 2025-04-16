59fc5a219c4b70d5e9287a407f9dd46d257f7795
EzeUG96 <166586628+EzeUG96@users.noreply.github.com>
2025-04-16 13:25:04 -0600

    Add files via upload
    "Deteccion.py" es el codigo que ejecuta el programa para la deteccion de billetes en videostreaming. "best.pt" es el modelo ya entrenado para la deteccion de billetes, este se carga en el codigo para su ejecución.

---
+++ Deteccion.py
@@ -0,0 +1,45 @@
+from ultralytics import YOLO
+import cv2
+import os
+
+# Obtener la ruta del script actual
+act_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Cargar el modelo entrenado
+modelo_path = os.path.join(act_dir, "runs/detect/train/weights/best.pt")  
+modelo = YOLO(modelo_path)  # Usar el modelo entrenado
+
+# Iniciar la cámara
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+# Verificar si la cámara se abre correctamente
+if not cap.isOpened():
+    print("No se pudo acceder a la cámara.")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("No se pudo recibir el frame de la cámara.")
+        break
+
+    # Realizar la predicción en cada frame
+    resultados = modelo(frame)
+
+    # Dibujar los resultados sobre el frame (aunque no haya detección, el frame se mostrará)
+    frame_anotado = resultados[0].plot() if len(resultados) > 0 else frame
+
+    # Mostrar la imagen anotada, aunque no haya detecciones
+    cv2.imshow("Predicción en vivo YOLOv8", frame_anotado)
+
+    # Salir si presionas 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Liberar recursos y cerrar la ventana
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
